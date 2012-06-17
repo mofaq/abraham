02cf77f8cabd74449f34f8b84926647309b23095
Simon Probert <simon.probert1@googlemail.com>
2012-06-18 09:37:23 +1000

    960 Template Fixes

--- 960/includes/js/init.js
+++ 960/includes/js/init.js
@@ -6,34 +6,61 @@ jQuery.event.add(window, "resize", initPanels);
 
 
 $(document).ready(function() {
-  $('li.primary-option').hover(
+  $('.primary-options li').hover(
 
-function(){$(this).stop().animate({ backgroundColor: "#b0b9e6", opacity: "0.75" }, 1)},
-function(){$(this).stop().animate({ backgroundColor: "white", opacity: "0.5" }, 800)}
+      function(){
+        $(this).stop().animate({ color:"#ffffff", backgroundColor: "#b0b9e6", opacity: "0.75" }, 1);
+        //$(this).$('.tiny').stop().animate({ color:"#ffffff" }, 1)
+      },
+      function(){
+        $(this).stop().animate({ color:"#000000",backgroundColor: "white", opacity: "0.65" }, 800);
+                //$(this).$('.tiny').stop().animate({ color:"#808080" }, 1)
+      }
+      
 )});
+
+
+
+$(document).ready(function() {
+  $('.selected').css('opacity','1.00');
+});
+
+$(document).ready(function() {
+  $('.switch').click(function() {
+  $("#language-selection").hide();
+  $("#option-selection").show();
+  });
+});
+
 
 
 function initPanels()
 {
-	var hh = 103
+	var hh = 109
 	var wh = $(window).height();
+	var ww = $(window).width();
 
   var ch = (wh-hh);
 	//$("#content").css('height',ph);
 
 
 	$("#sidebar").css('height',ch);
-	$("#panel_1").css('height',ch);
-	$("#panel_2").css('height',ch);
+	$("#left-sidebar1").css('height',ch);
+	$("#left-sidebar2").css('height',ch);
 
-	var pch = (ch-92)
+	$("#map").css('height',ch);
+	$("#map").css('width',(ww-381-256-2));
 
-	$(".panel-container").css('height',pch);
+	var pch = (ch-90)
+
+	$(".sidebar-container").css('height',pch);
 	$(".viewport").css('height',(pch));
 
   $('#scrollbar1').tinyscrollbar({ size: pch });
   $('#scrollbar2').tinyscrollbar({ size: pch });
-  //alert(pch);
+
+  $("#option-selection").hide();
+
 }
 
 //Fadeout the backgrounds on hover
